1c6304ce223c460f498d3be46b05ed0e4d90be06
mpinkert <mpinkert@wisc.edu>
2018-02-27 14:27:02 -0600

    Added calculatenumber of tiles function

--- mp_img_manip/tiling.py
+++ mp_img_manip/tiling.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def getTileStartEndIndex(tileNumber, tileSize, tileOffset = None, tileStepSize = None):
     #Calculate the starting and ending index along a single dimension for a tile
 
@@ -11,3 +13,21 @@ def getTileStartEndIndex(tileNumber, tileSize, tileOffset = None, tileStepSize =
     endIndex = startIndex + tileSize
     
     return (startIndex, endIndex)
+
+
+def calculateNumberOfTiles(sizeOfImageDimension, tileSize, tileStepSize = None):
+    border = 0
+    
+    if not tileStepSize:
+        tileStepSize = tileSize
+            
+    if tileSize > tileStepSize:
+        border = (tileSize - tileStepSize)
+
+    idxRange = sizeOfImageDimension-2*border
+
+    numberOfTiles = np.fix(idxRange/tileStepSize)
+    remainder = np.remainder(idxRange,tileStepSize)  
+    offset = np.fix(remainder/2) + border
+
+    return (numberOfTiles, offset)
