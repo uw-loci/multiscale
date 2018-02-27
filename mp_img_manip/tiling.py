8c558c8f7521c3f07fade82904b1deb8d76b62c8
mpinkert <mpinkert@wisc.edu>
2018-02-27 13:58:26 -0600

    Tile offset optional in get start end index
    Defaults to 0

--- mp_img_manip/tiling.py
+++ mp_img_manip/tiling.py
@@ -1,8 +1,11 @@
-def getTileStartEndIndex(tileNumber, tileSize, tileOffset, tileStepSize = None):
+def getTileStartEndIndex(tileNumber, tileSize, tileOffset = None, tileStepSize = None):
     #Calculate the starting and ending index along a single dimension for a tile
 
     if not tileStepSize:
         tileStepSize = tileSize
+        
+    if not tileOffset:
+        tileOffset = 0
             
     startIndex = ((tileNumber-1)*tileStepSize)+tileOffset
     endIndex = startIndex + tileSize
