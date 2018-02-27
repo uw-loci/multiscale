a11381e66d6c54d451a4913438ad3a7cb7096b1d
mpinkert <mpinkert@wisc.edu>
2018-02-27 14:35:14 -0600

    Tentative calculateRetardanceOverArea
    Untested

--- mp_img_manip/polarimetry.py
+++ mp_img_manip/polarimetry.py
@@ -1,2 +1,17 @@
-# -*- coding: utf-8 -*-
+def  calculateRetardanceOverArea(retardance, orientation)
+    
+    # This gives me the orientation in 360 degrees, doubled to calculate alignment.
+    circularOrientation = (2*pi/180)*(orientation/100);
+    complexOrientation = exp(1i*circularOrientation);
+    
+    retardanceWeightedByOrientation = retardance*complexOrientation;
+    
+    numPixels = np.size(retardance);
+    
+    averageRetardance = np.sum(retardanceWeightedByOrientation)/numPixels;
+    
+    retMag = np.absolute(averageRetardance);
+    retAngle = np.angle(averageRetardance)/2; 
+
+    return (retMag,retAngle)
 
