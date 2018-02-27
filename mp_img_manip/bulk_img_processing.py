8968e03bcbd009d7232e1e3a38b29156e66c8f1e
mpinkert <mpinkert@wisc.edu>
2018-02-27 17:06:55 -0600

    Implement find shared images off of basename
    New function to find images in two folders that share the same base name
    (string before the first underscore).
    
    Currently does not check for multiple files with differ suffixes, but
    same basenames.  Will just count all combinations.

--- mp_img_manip/bulk_img_processing.py
+++ mp_img_manip/bulk_img_processing.py
@@ -15,3 +15,32 @@ def getBaseFileName(fileName):
         return nameStr[0:underscoreIndexes]
     else:
         return nameStr
+
+def findSharedImages(dirOne, dirTwo):
+    """Base image names derived from directory one, are mapped to images from
+    directory two."""
+    
+    #todo: modify dirOne_baseFileNames into a list of names, and then
+    #implement a .txt file?
+    
+    #todo: make a check for different categories of name, if there are multiple instances of a single name?
+    
+    fileListOne = [f for f in os.listdir(dirOne) if f.endswith('.tif')]
+    fileListTwo = [f for f in os.listdir(dirTwo) if f.endswith('.tif')]
+        
+    dirOneImagePaths = list()
+    dirTwoImagePaths = list()
+
+    for i in range(0,len(fileListOne)):
+        baseNameOne = getBaseFileName(fileListOne[i])
+        
+        for j  in range(0,len(fileListTwo)):
+            baseNameTwo = getBaseFileName(fileListTwo[j])
+          
+            if baseNameOne == baseNameTwo:
+                
+                dirOneImagePaths.append(dirOne + fileListOne[i])
+                dirTwoImagePaths.append(dirTwo + fileListTwo[j])
+
+    
+    return (dirOneImagePaths, dirTwoImagePaths)
