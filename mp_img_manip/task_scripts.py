3311cd9ac128544c00597843a8f0201b98b72010
mpinkert <mpinkert@wisc.edu>
2018-03-06 16:24:53 -0600

    Syntactically complete supervisedRegister
    Untested

--- mp_img_manip/task_scripts.py
+++ mp_img_manip/task_scripts.py
@@ -6,21 +6,31 @@ Created on Tue Mar  6 15:52:18 2018
 """
 
 import SimpleITK as sitk
-
+import mp_img_manip.itkscripts as mitk
+import mp_img_manip.bulk_img_processing as blk
+import numpy as np
 
 def supervisedRegisterImages(fixedPath, movingPath):
     
-    
-    findOffset
-    
-    findSpacing
+    fixedImg = mitk.setupImg(fixedPath)
+    movingImg = mitk.setupImg(movingPath, setupOffset = True)
     
     goodRegister = False
     
     while not goodRegister:    
-        register(fixed, moving)
+        (transform, metric, optimizer) = mitk.affineRegister(fixedImg, movingImg)
+        goodRegister = mitk.askIfGoodRegister()
         
-        goodRegister = askIfGoodRegister()
+    registeredImg = sitk.Resample(movingImg, fixedImg, transform, sitk.sitkLinear, 0.0, movingImg.GetPixelID())    
     
-def registerBetweenDirectories():
+    return registeredImg
     
+
+def bulkSupervisedRegisterImages(fixedDir, movingDir, outputDir, outputSuffix):
+    
+    (fixedImgPathList, movingImgPathList) = blk.findSharedImages(fixedDir, movingDir)
+    
+    for i in range(0, np.size(fixedImgPathList)):
+        registeredImg = supervisedRegisterImages(fixedImgPathList[i], movingImgPathList[i])
+        registeredPath = blk.createNewImagePath(movingImgPathList[i], outputDir, outputSuffix)
+        sitk.WriteImage(registeredImg, registeredPath)
