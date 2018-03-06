c31d0841096d6d604d67c8034d13ea6b5d3a937b
mpinkert <mpinkert@wisc.edu>
2018-03-06 15:58:04 -0600

    started task scripts for supervised registration
    starting with syntax and fleshing out until it is actually code.

---
+++ mp_img_manip/task_scripts.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  6 15:52:18 2018
+
+@author: mpinkert
+"""
+
+import SimpleITK as sitk
+
+
+def supervisedRegisterImages(fixedPath, movingPath):
+    
+    
+    findOffset
+    
+    findSpacing
+    
+    goodRegister = False
+    
+    while not goodRegister:    
+        register(fixed, moving)
+        
+        goodRegister = askIfGoodRegister()
+    
+def registerBetweenDirectories():
+    
