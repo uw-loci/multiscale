16f9535131c6e30804be1db909036befe4bdbfb5
mpinkert <mpinkert@wisc.edu>
2018-03-07 15:35:30 -0600

    ask yes/no question function and getint function

--- mp_img_manip/utility_functions.py
+++ mp_img_manip/utility_functions.py
@@ -5,3 +5,28 @@ Created on Wed Mar  7 14:59:49 2018
 @author: mpinkert
 """
 
+def query_int(message):
+
+  while True:
+    try:
+       user_input = int(input(message))       
+    except ValueError:
+       print("Not an integer! Try again.")
+       continue
+    else:
+       return user_input 
+       break 
+
+
+def yes_no(question):
+    yes = set(['yes','y', 'ye', ''])
+    no = set(['no','n'])
+     
+    while True:
+        choice = input(question).lower()
+        if choice in yes:
+           return True
+        elif choice in no:
+           return False
+        else:
+           print("Please respond with 'yes' or 'no'\n")
