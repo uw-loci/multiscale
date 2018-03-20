6bf2f811da2cfb7cab8ffeb92062efd10a8a9dc4
mpinkert <mpinkert@wisc.edu>
2018-03-20 10:55:40 -0500

    Moved non-registraiton functions to new taskscript

---
+++ mp_img_manip/task_scripts/ps_mmp_shg_cw-ssim_analysis.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 20 10:50:24 2018
+
+@author: mpinkert
+"""
+
+
+
+def compare_ssim(dirOne, dirTwo):
+    """Compare SSIM between two file directories
+    """
+    return
+
+def downsample_ps_and_shg():
+    return
