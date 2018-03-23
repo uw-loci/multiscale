8d4e02f01fa0447ab9624465ec5cb4b3a27fc721
mpinkert <mpinkert@wisc.edu>
2018-03-23 10:58:33 -0500

    Moved dictionary definitions into own module

---
+++ mp_img_manip/dir_dictionary.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 10:56:37 2018
+
+@author: mpinkert
+
+My personal directories
+"""
+
+import os
+
+def create_dictionary(
+        base_dir = os.path.normpath('F:\Box Sync\Research\Polarimetry'),
+        prep_dir = '02 - Python prepped images',
+        resize_dir = '03 - Mid-python analysis images\\01 - Resizing images',
+        reg_dir = '03 - Mid-python analysis images\\02 - Registered images',
+        mask_dir = '03 - Mid-python analysis images\\03 - Masked images'):
+    
+    
+    dir_dict = {
+    "ps_large" : os.path.join(base_dir, prep_dir, 'PS_Large'),
+    "mmp_small" : os.path.join(base_dir, prep_dir, 'MMP_Small'),
+    "shg_large" : os.path.join(base_dir, prep_dir, 'SHG_Large'),
+    
+    "ps_small" : os.path.join(base_dir,resize_dir,'PS_Small'),
+    "shg_small" : os.path.join(base_dir,resize_dir,'SHG_Small'),
+    "mmp_large" : os.path.join(base_dir,resize_dir,'MMP_Large'),
+                                
+    "mmp_small_reg" : os.path.join(base_dir,reg_dir,'MMP_Small_Reg'),
+    "shg_small_reg" : os.path.join(base_dir,reg_dir,'SHG_Small_Reg'),
+
+    "mmp_large_reg" : os.path.join(base_dir,reg_dir,'MMP_Large_Reg'),
+    "shg_large_reg" : os.path.join(base_dir,reg_dir,'SHG_Large_Reg'),
+
+    "ps_small_mask" : os.path.join(base_dir,mask_dir,'PS_Small_Mask'),
+    "shg_small_mask" : os.path.join(base_dir,mask_dir,'SHG_Small_Mask'),
+
+    "ps_large_mask" : os.path.join(base_dir,mask_dir,'PS_Large_Mask'),
+    "shg_large_mask" : os.path.join(base_dir,mask_dir,'SHG_Large_Mask')}
+
+
+    for key in dir_dict: os.makedirs(dir_dict[key], exist_ok = True)
+    
+    return dir_dict    
--- mp_img_manip/task_scripts/ps_mmp_shg_registration.py
+++ mp_img_manip/task_scripts/ps_mmp_shg_registration.py
@@ -10,6 +10,7 @@ import os
 import mp_img_manip.itk.registration as reg
 import mp_img_manip.itk.transform as trans
 import mp_img_manip.itk.process as proc
+import mp_img_manip.dir_dictionary as dird
 
 def perform_registrations():
     """Overall script to perform both mmp and shg registrations
@@ -20,7 +21,7 @@ def perform_registrations():
     02 - Registered images
     03 - Registered images maskped to MMP boundaries"""
     
-    dir_dict = create_dictionary()
+    dir_dict = dird.create_dictionary()
       
     resize_images(dir_dict)
 
@@ -31,42 +32,6 @@ def perform_registrations():
     mask_large_images(dir_dict)
     
     
-
-def create_dictionary(
-        base_dir = os.path.normpath('F:\Box Sync\Research\Polarimetry'),
-        prep_dir = '02 - Python prepped images',
-        resize_dir = '03 - Mid-python analysis images\\01 - Resizing images',
-        reg_dir = '03 - Mid-python analysis images\\02 - Registered images',
-        mask_dir = '03 - Mid-python analysis images\\03 - Masked images'):
-    
-    
-    dir_dict = {
-    "ps_large" : os.path.join(base_dir, prep_dir, 'PS_Large'),
-    "mmp_small" : os.path.join(base_dir, prep_dir, 'MMP_Small'),
-    "shg_large" : os.path.join(base_dir, prep_dir, 'SHG_Large'),
-    
-    "ps_small" : os.path.join(base_dir,resize_dir,'PS_Small'),
-    "shg_small" : os.path.join(base_dir,resize_dir,'SHG_Small'),
-    "mmp_large" : os.path.join(base_dir,resize_dir,'MMP_Large'),
-                                
-    "mmp_small_reg" : os.path.join(base_dir,reg_dir,'MMP_Small_Reg'),
-    "shg_small_reg" : os.path.join(base_dir,reg_dir,'SHG_Small_Reg'),
-
-    "mmp_large_reg" : os.path.join(base_dir,reg_dir,'MMP_Large_Reg'),
-    "shg_large_reg" : os.path.join(base_dir,reg_dir,'SHG_Large_Reg'),
-
-    "ps_small_mask" : os.path.join(base_dir,mask_dir,'PS_Small_Mask'),
-    "mmp_small_mask" : os.path.join(base_dir,mask_dir,'MMP_Small_Mask'),
-    "shg_small_mask" : os.path.join(base_dir,mask_dir,'SHG_Small_Mask'),
-
-    "ps_large_mask" : os.path.join(base_dir,mask_dir,'PS_Large_Mask'),
-    "mmp_large_mask" : os.path.join(base_dir,mask_dir,'MMP_Large_Mask'),
-    "shg_large_mask" : os.path.join(base_dir,mask_dir,'SHG_Large_Mask')}
-
-
-    for key in dir_dict: os.makedirs(dir_dict[key], exist_ok = True)
-    
-    return dir_dict    
 
 
 
