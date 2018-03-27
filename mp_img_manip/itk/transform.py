636d02a98b41e1420828ba193cd9911e31cb98ba
mpinkert <mpinkert@wisc.edu>
2018-03-27 14:05:29 -0500

    implemented bulk apply transform
    also fixed various bugs, such as in apply_transform, and not setting
    origin correctly in resize image

--- mp_img_manip/itk/transform.py
+++ mp_img_manip/itk/transform.py
@@ -6,6 +6,7 @@ Created on Wed Mar 21 09:39:34 2018
 """
 import mp_img_manip.bulk_img_processing as blk
 import mp_img_manip.itk.metadata as meta 
+import mp_img_manip.utility_functions as util
 
 import SimpleITK as sitk
 import numpy as np
@@ -37,6 +38,10 @@ def apply_transform(fixed_path, moving_path, reference_path):
     moving_image = meta.setup_image(moving_path, setup_origin = True)
     
     (reference_dir, reference_name) = os.path.split(reference_path)
+
+    print('Applying transform onto {0} based on transform on {1}'.format(
+            os.path.basename(moving_path),
+            reference_name))
     
     transform_path = os.path.join(reference_dir,'Transforms.csv')
     
@@ -44,48 +49,67 @@ def apply_transform(fixed_path, moving_path, reference_path):
                                            reference_name,'Image')
     
     transform = sitk.AffineTransform(2)
-    transform.SetMatrix(transform_params['Matrix Top Left'],
-                        transform_params['Matrix Top Right'],
-                        transform_params['Matrix Bottom Left'],
-                        transform_params['Matrix Bottom Right'])
+    matrix = [transform_params['Matrix Top Left'],
+               transform_params['Matrix Top Right'], 
+               transform_params['Matrix Bottom Left'],
+               transform_params['Matrix Bottom Right']]
     
-    transform.SetOrigin(transform_params['X Translation'],
-                        transform_params['Y Translation'])
+    transform.SetMatrix(matrix)
+    transform.SetTranslation([transform_params['X Translation'],
+                        transform_params['Y Translation']])
     
     return sitk.Resample(moving_image, fixed_image, transform, 
                          sitk.sitkLinear, 0.0, moving_image.GetPixelID())
     
     
 
-def bulk_apply_transform(fixed_dir, moving_dir, output_dir,
-                         transform_dir,
-                         output_suffix,):
+def bulk_apply_transform(fixed_dir, moving_dir, transform_dir, 
+                         output_dir, output_suffix):
     
+    fixed_paths, moving_paths, transform_paths = blk.find_bulk_shared_images(
+            [fixed_dir, moving_dir, transform_dir])
+    
+    
+    for i in range(0, np.size(fixed_paths)):
+        
+        registered_image = apply_transform(fixed_paths[i],
+                                           moving_paths[i],
+                                           transform_paths[i])
+        
+        registered_path = blk.create_new_image_path(moving_paths[i],
+                                                    output_dir,
+                                                    output_suffix)
+        
+        sitk.WriteImage(registered_image, registered_path)
+        meta.write_image_parameters(registered_path,
+                                    registered_image.GetSpacing(),
+                                    registered_image.GetOrigin())
+        
     return
 
 
 def resize_image(image_path, output_suffix, current_spacing, target_spacing):
     """Resize an image by an integer factor towards target spacing"""
-    itkImg = meta.setup_image(image_path, return_image = True, print_parameters = False)
+    itk_image = meta.setup_image(image_path,
+                                 return_image = True, print_parameters = False)
     
     image_name = os.path.basename(image_path)
-    
-    
-    
     
     if current_spacing < target_spacing: 
         scale = math.floor(target_spacing/current_spacing)
         end_res = current_spacing*scale
         
-        resized_image = sitk.Shrink(itkImg,[scale,scale])
+        resized_image = sitk.Shrink(itk_image,[scale,scale])
         resized_image.SetSpacing([end_res,end_res])
-    
+        resized_image.SetOrigin(itk_image.GetOrigin())
+        
     elif current_spacing > target_spacing:
         scale = math.floor(current_spacing/target_spacing)
         end_res = current_spacing/scale
         
-        resized_image = sitk.Expand(itkImg,[scale,scale])
+        resized_image = sitk.Expand(itk_image,[scale,scale])
         resized_image.SetSpacing([end_res,end_res])
+        resized_image.SetOrigin(itk_image.GetOrigin())
     
     print('Resizing ' + image_name + ' from ' 
           + str(current_spacing) + ' to ' + str(end_res) 
@@ -93,6 +117,7 @@ def resize_image(image_path, output_suffix, current_spacing, target_spacing):
     
     return resized_image
     
+
 def bulk_resize_image(fixed_dir, moving_dir, output_dir, output_suffix):
     """Resize multiple images to corresponding reference size"""
     (fixed_image_path_list, moving_image_path_list) = blk.find_shared_images(
