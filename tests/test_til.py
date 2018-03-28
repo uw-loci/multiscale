3b83393b39449ed4f48e697e2b416fbcf06176fa
mpinkert <mpinkert@wisc.edu>
2018-03-28 16:16:00 -0500

    started unit test file for tiling

---
+++ tests/test_til.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 28 16:13:51 2018
+
+@author: mpinkert
+"""
+
+import unittest
+
+import mp_img_manip.tiling as til
+
+class tile_passes_threshold_test_suite(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_absolute_truth_and_meaning(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
