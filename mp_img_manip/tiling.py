7c51eee82332bf40a0c9d4f63c61747b47e41491
mpinkert <mpinkert@wisc.edu>
2018-03-29 11:29:28 -0500

    started auto extract tiles functions

--- mp_img_manip/tiling.py
+++ mp_img_manip/tiling.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import SimpleITK as sitk
+import os
+import mp_img_manip.utility_functions as util
 
 def get_tile_start_end_index(tile_number, tile_size,
                              tile_offset = None, tile_step_size = None):
@@ -41,6 +43,7 @@ def calculate_number_of_tiles(size_of_image_dimension, tile_size,
 
     return int(number_of_tiles), int(offset)
 
+
 def tile_passes_threshold(tile, val_threshold, num_threshold):
     """Given a np array, check if it has enough entries larger than a value"""
     
@@ -53,3 +56,42 @@ def tile_passes_threshold(tile, val_threshold, num_threshold):
     else:
         return False
     
+    
+def query_tile_size_and_separation(diff_separation = False):
+    message_tile_size = 'How many pixels should the tile width/height be?'
+    tile_size = util.query_int(message_tile_size)
+
+
+    if diff_separation:       
+        message_separation = 'What is the separation between tiles?'
+        separation = util.query_int(message_separation)
+    else:
+        separation = tile_size
+
+    
+    return tile_size, separation
+
+
+def query_tile_thresholds():
+    message_intensity = 'What percentage intensity is the value threshold?'
+    message_number = 'What percentage of pixels need to be above the threshold?'
+    
+    intensity_threshold = util.query_int(message_intensity)
+    number_threshold = util.query_int(message_number)
+    
+    return intensity_threshold, number_threshold
+
+
+
+
+def extract_image_tiles(image_path, output_dir, output_suffix,
+                        diff_separation = False):
+    
+    basename = os.path.basename(image_path)
+    print('Extracting tiles from {0}'.format(basename))
+    
+    input_image = sitk.ReadImage(image_path)
+    input_array = sitk.GetArrayFromImage(input_image)
+    
+    image_dimens = np.shape(input_array)
+    tile_size, separation = query_tile_size_and_separation(diff_separation)
