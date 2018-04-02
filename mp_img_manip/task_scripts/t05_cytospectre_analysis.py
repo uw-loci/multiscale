fc6d9e761f025c61c6edabfb9b1378926e8a216b
mpinkert <mpinkert@wisc.edu>
2018-04-02 16:09:47 -0500

    script to cytospectre analysis, unimplemented

---
+++ mp_img_manip/task_scripts/t05_cytospectre_analysis.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  2 16:03:35 2018
+
+@author: mpinkert
+"""
+
+import mp_img_manip.dir_dictionary as dird
+import mp_img_manip.bulk_img_processing as blk
+
+def compare_like_rois(sample_dir, output_dir, output_suffix):
+    
+    return
+
+def bulk_compare_like_rois():
+    return
