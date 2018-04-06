6e63ab2c72807e125f05ec9776c5da20c27e4325
mpinkert <mpinkert@wisc.edu>
2018-04-06 15:43:55 -0500

    test file and test module for cytospectre

--- mp_img_manip/task_scripts/t05_cytospectre_analysis.py
+++ mp_img_manip/task_scripts/t05_cytospectre_analysis.py
@@ -9,45 +9,65 @@ import mp_img_manip.dir_dictionary as dird
 import mp_img_manip.bulk_img_processing as blk
 import mp_img_manip.utility_functions as util
 import pandas as pd
+import os
 
 
 def parse_index(roi_str):
     
-    
+    sample, modality, roi = blk.file_name_parts(roi_str)
     return sample, modality, roi
 
 
 def dataframe_generator(analysis_list):
     
-    index = 'Image'
-    relevant_cols = ['Image', 'Mean orientation', 'Circ. variance']
-    
+
     #read in the dataframes
     for item in analysis_list:
            yield pd.read_excel(item, usecols = relevant_cols, index_col = index)
     
         
 
-
-
-
-def clean_up_dataframes(analysis_list):
-    dirty_dataframes = blk.dataframe_generator_excel(analysis_list)
-
+def clean_single_dataframe(dirty_frame):
     index = ['Sample', 'ROI', 'Variable']
-    column_labels = ['PS', 'SHG', 'MMP']
-        
+    column_labels = ['PS', 'SHG', 'MMP']     
     clean_dataframe = pd.DataFrame(columns = column_labels)
     clean_dataframe.set_index(index)
     
-    for frame in dirty_dataframes:
-        for index, row in frame.iterrows():
-            sample, modality, roi = parse_index(index)
-            clean_dataframe.loc[(sample, roi,'Orientation'), modality] = row[0]
-            clean_dataframe.loc[(sample, roi,'Alignment'), modality] = row[1]
-            
-    return clean_dataframe
     
+    
+    
+    #The current loop is low efficiency, as it is value-wise changes and pandas
+    #Copies the whole dataframe every operation
+    
+    
+    for index, row in dirty_frame.iterrows():
+        sample, modality, roi = parse_index(index)
+        clean_dataframe.loc[(sample, roi,'Orientation'), modality] = row[0]
+        clean_dataframe.loc[(sample, roi,'Alignment'), modality] = row[1]
+        
+    return clean_dataframe
+
+def clean_up_dataframes(analysis_list):
+    
+    dirty_index = 'Image'
+    relevant_cols = ['Image', 'Mean orientation', 'Circ. variance']
+    dirty_dataframes = blk.dataframe_generator_excel(analysis_list, 
+                                                     dirty_index,
+                                                     relevant_cols)
+
+    index = ['Sample', 'ROI', 'Variable']
+    column_labels = ['PS', 'SHG', 'MMP']     
+    clean_dataframes = pd.DataFrame(columns = column_labels)
+    clean_dataframes.set_index(index)
+
+    for dirty_frame in dirty_dataframes:
+        dataframe = clean_single_dataframe(dirty_frame)
+        clean_dataframes.append(dataframe)   
+        
+    return clean_dataframes
+    
+
+
 def write_roi_comparison_file(sample_dir, output_dir, output_suffix):
     
     analysis_list = util.list_filetype_in_dir(sample_dir, '.csv')
---
+++ tests/test_cyto.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 15:43:03 2018
+
+@author: mpinkert
+"""
+
+import mp_img_manip.cytospectre as cyto
+import unittest
+
+class BasicTestSuite(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_absolute_truth_and_meaning(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
