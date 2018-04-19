103d00ae856ee46b280f294f8f3485568a048731
mpinkert <mpinkert@wisc.edu>
2018-04-19 12:06:52 -0500

    auto ssim for physical mmp/ps, 8bit else

--- mp_img_manip/task_scripts/t03_ssim.py
+++ mp_img_manip/task_scripts/t03_ssim.py
@@ -5,3 +5,20 @@ Created on Wed Mar 28 12:58:50 2018
 @author: mpinkert
 """
 
+import mp_img_manip.cw_ssim as ssim
+import mp_img_manip.dir_dictionary as dird
+
+dir_dict = dird.create_dictionary()
+
+mmp_ps = [dir_dict['mmp_small_reg'], dir_dict['ps_small_mask']]
+mmp_shg = [dir_dict['mmp_small_8bit'], dir_dict['shg_small_8bit']]
+shg_ps = [dir_dict['shg_small_8bit'], dir_dict['ps_small_8bit']]
+
+
+
+output_name = 'CW-SSIM Small physical mmp_ps 8bit else.csv'
+
+ssim.bulk_compare_ssim(mmp_ps, dir_dict['ssim'], output_name)
+ssim.bulk_compare_ssim(mmp_shg, dir_dict['ssim'], output_name)
+ssim.bulk_compare_ssim(shg_ps, dir_dict['ssim'], output_name)
+
