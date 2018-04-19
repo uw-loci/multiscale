969bca1b1ec5e31fb0ba0c3b58f138db4ac89a5a
mpinkert <mpinkert@wisc.edu>
2018-04-19 12:07:12 -0500

    inte thresh 5, takes from mask/reg not 8bit

--- mp_img_manip/task_scripts/t04_cytospectre_prep.py
+++ mp_img_manip/task_scripts/t04_cytospectre_prep.py
@@ -13,24 +13,24 @@ dir_dict = dird.create_dictionary()
 
 
 def extract_tiles_from_8bit_images(tile_size = 512,
-                                   intensity_threshold = 1, 
+                                   intensity_threshold = 5, 
                                    number_threshold = 10):
     
-    til.bulk_extract_image_tiles(dir_dict['ps_large_8bit'], 
+    til.bulk_extract_image_tiles(dir_dict['ps_large_mask'], 
                                  dir_dict['ps_large_tile'], 
                                  'PS',
                                  tile_size = tile_size, 
                                  intensity_threshold = intensity_threshold, 
                                  number_threshold = number_threshold)
     
-    til.bulk_extract_image_tiles(dir_dict['shg_large_8bit'], 
+    til.bulk_extract_image_tiles(dir_dict['shg_large_mask'], 
                                  dir_dict['shg_large_tile'], 
                                  'SHG',
                                  tile_size = tile_size, 
                                  intensity_threshold = intensity_threshold, 
                                  number_threshold = number_threshold)
     
-    til.bulk_extract_image_tiles(dir_dict['mmp_large_8bit'], 
+    til.bulk_extract_image_tiles(dir_dict['mmp_large_reg'], 
                                  dir_dict['mmp_large_tile'], 
                                  'MMP',
                                  tile_size = tile_size, 
