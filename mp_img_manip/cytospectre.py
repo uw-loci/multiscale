9881d5e1b3415ddf03d548427eeb75a5a00044d6
mpinkert <mpinkert@wisc.edu>
2018-04-26 09:32:37 -0500

    deleted unused code/old functions

--- mp_img_manip/cytospectre.py
+++ mp_img_manip/cytospectre.py
@@ -7,7 +7,6 @@ Created on Fri Apr  6 15:42:22 2018
 
 
 import mp_img_manip.bulk_img_processing as blk
-import mp_img_manip.utility_functions as util
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -17,6 +16,7 @@ def parse_index(roi_str):
     
     sample, modality, thresholds, roi = blk.file_name_parts(roi_str)
     return sample, modality, thresholds, roi
+
 
 def bulk_parse_index(roi_list):
     indices_list = blk.file_name_parts_list(roi_list)
@@ -51,8 +51,6 @@ def clean_single_dataframe(dirty_frame):
     return clean_frame
 
 
-
-
 def clean_multiple_dataframes(analysis_list, output_dir, output_suffix):
     
     dirty_index = 'Image'
@@ -69,35 +67,3 @@ def clean_multiple_dataframes(analysis_list, output_dir, output_suffix):
         
         clean_dataframe.to_csv(output_path)
     
-
-
-#
-#def write_roi_comparison_file(sample_dir, output_dir = None,
-#                              output_suffix = 'Cleaned data.csv'):
-#    """Deprecated"""
-#    analysis_list = util.list_filetype_in_dir(sample_dir, '.xls')
-#    
-#    if output_dir is None:
-#        output_path = Path(sample_dir, output_suffix)
-#    else:    
-#        output_path = Path(output_dir, output_suffix)
-#    
-#    try:
-#        clean_dataframe = pd.read_csv(output_path, 
-#                                      header = [0, 1], index_col = [0, 1, 2])
-#    except:
-#        dirty_frame = pd.read_excel(analysis_list[0],
-#                                    index_col = 'Image')
-#        clean_dataframe = clean_single_dataframe(dirty_frame)
-#        analysis_list.pop(0)        
-#    
-#    
-#    output_frame = clean_multiple_dataframes(analysis_list, clean_dataframe)
-
-
-
-def plot_roi_comparison():
-    return
-
-def r2_roi_comparison():
-    return
