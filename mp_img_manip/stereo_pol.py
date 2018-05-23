5ffb5cb9a0409393944a3482ae041d27e8f0fc68
mpinkert <mpinkert@wisc.edu>
2018-05-23 09:59:13 -0500

    added javabridge start/kill

--- mp_img_manip/stereo_pol.py
+++ mp_img_manip/stereo_pol.py
@@ -1,4 +1,7 @@
-import python-bioformats as bf
+import javabridge
+import bioformats as bf
+javabridge.start_vm(class_path=bioformats.JARS)
+
 
 def process_czi_file(czi_file_path, flat_field_path):
 
@@ -18,9 +21,14 @@ def process_czi_file(czi_file_path, flat_field_path):
 
 def get_stage_position(stack):
 
+
+
 def correct_intensity(stack, flat_field_path):
 
 
 def format_positions_for_stitching(stage_position_list):
 
 
+
+
+javabridge.kill_vm()
