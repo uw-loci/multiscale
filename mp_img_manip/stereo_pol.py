accda502f7496f90d88df3f1067ad5cb5d83e8db
mpinkert <mpinkert@wisc.edu>
2018-05-30 10:18:53 -0500

    stereo pol unused and commented out
    Using an imagej and matlab script for same purpose, but may drag files
    back into use later

--- mp_img_manip/stereo_pol.py
+++ mp_img_manip/stereo_pol.py
@@ -1,34 +1,34 @@
-import javabridge
-import bioformats as bf
-javabridge.start_vm(class_path=bioformats.JARS)
-
-
-def process_czi_file(czi_file_path, flat_field_path):
-
-    czi_file_stacks = bf.open(czi_file_path)
-
-    stage_position_list = []
-
-    for stack in czi_file_stacks:
-        stage_position_list.append(get_stage_position(stack))
-
-        stack_intensity_corrected = correct_intensity(stack, flat_field_path)
-
-        save_new_stack(stack_intensity_corrected)
-
-    format_positions_for_stitching(stage_position_list)
-
-
-def get_stage_position(stack):
-
-
-
-def correct_intensity(stack, flat_field_path):
-
-
-def format_positions_for_stitching(stage_position_list):
-
-
-
-
-javabridge.kill_vm()
+# import javabridge
+# import bioformats as bf
+# javabridge.start_vm(class_path=bioformats.JARS)
+#
+#
+# def process_czi_file(czi_file_path, flat_field_path):
+#
+#     czi_file_stacks =
+#
+#     stage_position_list = []
+#
+#     for stack in czi_file_stacks:
+#         stage_position_list.append(get_stage_position(stack))
+#
+#         stack_intensity_corrected = correct_intensity(stack, flat_field_path)
+#
+#         save_new_stack(stack_intensity_corrected)
+#
+#     format_positions_for_stitching(stage_position_list)
+#
+#
+# def get_stage_position(stack):
+#
+#
+#
+# def correct_intensity(stack, flat_field_path):
+#
+#
+# def format_positions_for_stitching(stage_position_list):
+#
+#
+#
+#
+# javabridge.kill_vm()
