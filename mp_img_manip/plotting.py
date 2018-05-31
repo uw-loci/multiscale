873e4dbf5d603b99837d59794cc9d7e4c2a4e724
mpinkert <mpinkert@wisc.edu>
2018-05-31 10:00:46 -0500

    function in plotting for colored overlay
    As in last commit, but the actual function to do so

--- mp_img_manip/plotting.py
+++ mp_img_manip/plotting.py
@@ -7,6 +7,25 @@ Created on Thu Apr 12 09:25:11 2018
 import matplotlib.pyplot as plt
 import numpy as np
 
+def plot_colored_overlay(array_one, array_two, difference_threshold=0.1):
+    """Plot two same-size images in 3 channels, with blue->same position"""
+    
+    diff = array_one - array_two
+    abs_diff = np.abs(diff)    
+    dims = np.shape(diff)
+    
+    grayscale = abs_diff < difference_threshold
+    red = diff > 0.1
+    green = diff < -0.1
+
+    rgb_overlay = np.zeros(shape=(dims[0], dims[1], 3), dtype=float)
+    
+    rgb_overlay[grayscale, 2] = np.ma.masked_array(array_one[grayscale])
+    rgb_overlay[red, 0] = np.ma.masked_array(array_one[red])
+    rgb_overlay[green, 1] = np.ma.masked_array(array_two[green])
+    
+    return rgb_overlay
+    
 
 def bland_altman_plot(data1, data2, *args, **kwargs):
     data1     = np.asarray(data1)
