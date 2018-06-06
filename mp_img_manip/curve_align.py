f886db91ca4eee86bc534502500fd26d896d7b44
mpinkert <mpinkert@wisc.edu>
2018-06-06 10:40:36 -0500

    new module for curvealign preparation
    empty

---
+++ mp_img_manip/curve_align.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  6 10:19:59 2018
+
+@author: mpinkert
+"""
+
+import mp_img_manip.bulk_img_processing as blk
+import pandas as pd
+import numpy as np
+import scipy.io as sio
+from pathlib import Path
+
+
