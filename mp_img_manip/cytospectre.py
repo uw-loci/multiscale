6eddc6d399bee56c53f521061510734b6f2d55e0
mpinkert <mpinkert@wisc.edu>
2018-06-07 14:29:27 -0500

    changed ROI label -> Tile
    So Cytospectre/CurveAlign tiles can communicate, and still have CA ROIs
    to analyze

--- mp_img_manip/cytospectre.py
+++ mp_img_manip/cytospectre.py
@@ -51,7 +51,7 @@ def parsed_indices_to_pd_index(parsed_indices):
     """ Take a list of parsed indices and turn them into a pandas multi-index
     """ 
     
-    pre_variable_sort_index = ['Sample', 'Modality', 'Thresholds', 'ROI']
+    pre_variable_sort_index = ['Sample', 'Modality', 'Thresholds', 'Tile']
     transposed_indices = np.transpose(parsed_indices)
     clean_index_stacked = pd.MultiIndex.from_arrays(transposed_indices,
                                           names = pre_variable_sort_index)
@@ -79,7 +79,7 @@ def clean_single_dataframe(dirty_frame):
     cluttered_index = parsed_indices_to_pd_index(parsed_indices)  
     cluttered_frame = cutdown_frame.set_index(cluttered_index)
     flat_frame = cluttered_frame.reset_index()
-    clean_frame = pd.pivot_table(flat_frame, index=['Sample', 'ROI'], 
+    clean_frame = pd.pivot_table(flat_frame, index=['Sample', 'Tile'],
                                  values=['Alignment', 'Orientation'],
                                  columns = 'Modality')
     
