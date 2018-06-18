6ed589adc6bb2a9a97ef659024870e029ab6dc4b
mpinkert <mpinkert@wisc.edu>
2018-06-18 16:01:11 -0500

    analyze func to get tile and roi dataframes

--- mp_img_manip/task_scripts/t03_curve_align_analysis.py
+++ mp_img_manip/task_scripts/t03_curve_align_analysis.py
@@ -1,6 +1,9 @@
-from pathlib import Path
 import mp_img_manip.curve_align as ca
 import mp_img_manip.dir_dictionary as dird
+import mp_img_manip.utility_functions as util
+
+from functools import reduce
+import pandas as pd
 import datetime
 
 date = str(datetime.date.today())
@@ -14,4 +17,21 @@ def compile_results(dir_dict):
     ca.scrape_results(dir_dict['curve'], 'PS', 'PS_' + date)
 
 def analyze_results(dir_dict):
-    dataframe
+    
+    csv_list = util.list_filetype_in_dir(dir_dict['curve'], 'csv')
+    df_list = [pd.read_csv(csv) for csv in csv_list]
+    clean_list = [pd.pivot_table(flat_frame, index=['Sample', 'Tile','ROI'],
+                                 values=['Alignment', 'Orientation'],
+                                 columns = 'Modality') for flat_frame in df_list]
+    
+    
+    clean_df = reduce(lambda x, y: pd.merge(x, y, left_index=True, right_index=True), clean_list)
+    
+    tile_df = clean_df.xs('Full-tile', level='ROI')
+    roi_df = clean_df.drop('Full-tile', level=2)
+    
+    return tile_df, roi_df
+    
+
+#compile_results(dir_dict)
+tile_df, roi_df = analyze_results(dir_dict)
