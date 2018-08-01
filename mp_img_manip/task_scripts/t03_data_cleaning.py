95ead62faa12053fb0df93a7827d422a53d9ac26
mpinkert <mpinkert@wisc.edu>
2018-08-01 11:41:41 -0500

    separated clean ca tile and roi
    As their interdepence was causing issues due to different indexes

--- mp_img_manip/task_scripts/t03_data_cleaning.py
+++ mp_img_manip/task_scripts/t03_data_cleaning.py
@@ -14,37 +14,46 @@ dir_dict = dird.create_dictionary()
 
 
 def compile_results(dir_dict):
-#    ca.scrape_results(dir_dict['curve'], 'SHG', 'SHG_' + date)
+    ca.scrape_results(dir_dict['curve'], 'SHG', 'SHG_' + date)
     ca.scrape_results(dir_dict['curve'], 'MLR', 'MLR_' + date)
     ca.scrape_results(dir_dict['curve'], 'MHR', 'MHR_' + date)
 #    ca.scrape_results(dir_dict['curve'], 'PS', 'PS_' + date)
 
 
-def clean_curve_align_results(dir_dict):
+def clean_curve_align_tiles(dir_dict):
+    csv_list = util.list_filetype_in_dir(Path(dir_dict['curve'], 'Tile'), 'csv')
+    df_list = [pd.read_csv(csv) for csv in csv_list]
+    clean_list = [pd.pivot_table(flat_frame, index=['Mouse', 'Slide', 'Tile'],
+                                 values=['Alignment', 'Orientation'],
+                                 columns = 'Modality') for flat_frame in df_list]
+
+    clean_tile_df = reduce(lambda x, y: pd.concat([x, y], axis=1), clean_list)
     
-    csv_list = util.list_filetype_in_dir(dir_dict['curve'], 'csv')
+    return clean_tile_df
+
+    
+def clean_curve_align_rois(dir_dict):
+       
+    csv_list = util.list_filetype_in_dir(Path(dir_dict['curve'], 'ROI'), 'csv')
     df_list = [pd.read_csv(csv) for csv in csv_list]
     clean_list = [pd.pivot_table(flat_frame, index=['Mouse', 'Slide', 'Tile','ROI'],
                                  values=['Alignment', 'Orientation'],
                                  columns = 'Modality') for flat_frame in df_list]
     
     
-    clean_df = reduce(lambda x, y: pd.concat([x, y], axis=1), clean_list)
+    clean_roi_df = reduce(lambda x, y: pd.concat([x, y], axis=1), clean_list)
     
-    tile_df = clean_df.xs('Full-tile', level='ROI')
-    roi_df = clean_df.drop('Full-tile', level=2)
-    
-    return tile_df, roi_df
+    return clean_roi_df
     
 
 def clean_cytospectre_results(dir_dict):
     xls_list = util.list_filetype_in_dir(dir_dict['cyto'], 'xls')
 
-    dirty_df_list = [pd.read_excel(xls_file, index_col='Image') for xls_file in xls_list] 
+    df_list_dirty = [pd.read_excel(xls_file, index_col='Image') for xls_file in xls_list]
     
-    clean_df_list = [cyto.clean_single_dataframe(dirty_df) for dirty_df in dirty_df_list]
+    df_list_clean = [cyto.clean_single_dataframe(dirty_df) for dirty_df in df_list_dirty]
     
-    clean_df = reduce(lambda x, y: pd.merge(x, y, left_index=True, right_index=True), clean_df_list)
+    clean_df = reduce(lambda x, y: pd.merge(x, y, left_index=True, right_index=True), df_list_clean)
     
     return clean_df
 
@@ -52,17 +61,18 @@ def clean_cytospectre_results(dir_dict):
 # Sample workflow.  Take care, compile_results takes a long time and if run twice on the same date
     # Will duplicate results.
 
-#compile_results(dir_dict)
-tile_df, roi_df = clean_curve_align_results(dir_dict)
-cyto_df = clean_cytospectre_results(dir_dict)
+compile_results(dir_dict)
+df_tile = clean_curve_align_tiles(dir_dict)
+df_roi = clean_curve_align_rois(dir_dict)
+df_cyto = clean_cytospectre_results(dir_dict)
 
-tile_path = Path('F:\Research\Polarimetry\Data 04 - Analysis results and graphics',
-                 'Curve-Align_Tiles_' + date + '.csv')
-roi_path = Path('F:\Research\Polarimetry\Data 04 - Analysis results and graphics',
-                'Curve-Align_ROIs_' + date + '.csv')
-cyto_path = Path('F:\Research\Polarimetry\Data 04 - Analysis results and graphics',
-                 'Cytospectre_Tiles_' + date + '.csv')
+path_tile = Path('F:\Research\Polarimetry\Data 04 - Analysis results and graphics',
+                 'Curve-Align_Tiles.csv')
+path_roi = Path('F:\Research\Polarimetry\Data 04 - Analysis results and graphics',
+                'Curve-Align_ROIs.csv')
+path_cyto = Path('F:\Research\Polarimetry\Data 04 - Analysis results and graphics',
+                 'Cytospectre_Tiles.csv')
 
-tile_df.to_csv(tile_path)
-roi_df.to_csv(roi_path)
-cyto_df.to_csv(cyto_path)
+df_tile.to_csv(path_tile)
+df_roi.to_csv(path_roi)
+df_cyto.to_csv(path_cyto)
