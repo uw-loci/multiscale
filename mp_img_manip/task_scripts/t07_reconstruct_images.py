e8962e8a85fedbc718c899ceac9eab0e0a58e3bd
mpinkert <mpinkert@wisc.edu>
2018-08-02 15:55:46 -0500

    bulk construct works
    Due to a fixed image dimensions function, and isnull checks for 'empty'
    values

--- mp_img_manip/task_scripts/t07_reconstruct_images.py
+++ mp_img_manip/task_scripts/t07_reconstruct_images.py
@@ -7,33 +7,63 @@ Created on Fri Jul 13 14:53:54 2018
 import pandas as pd
 import os
 from pathlib import Path
+import matplotlib.pyplot as plt
+import SimpleITK as sitk
+import numpy as np
 
 import mp_img_manip.analysis as an
 import mp_img_manip.plotting as myplot
 import mp_img_manip.utility_functions as util
 import mp_img_manip.tiling as til
-
+import mp_img_manip.bulk_img_processing as blk
+import mp_img_manip.dir_dictionary as dird
 
 
 def bulk_construct_images(df_single_modality_variable, modality, dir_modality,
                           dir_output, suffix_output):
-    
+
     for grp, df in df_single_modality_variable.groupby(['Mouse', 'Slide']):
-        
-        path_to_image = find_matching_image(grp, dir_modality)
-        dimensions = util.get_image_size(path_to_image)
-        image_array = til.roi_values_to_image(df, dimensions, modality)
-        
-        write_image(image_array, dir_output, suffix_output)
-        
+
+        if df.isnull().all():
+            continue
+
+        path_to_image = Path(dir_modality, find_matching_image(grp, dir_modality))
+        dimensions = get_image_dimensions(path_to_image)
+        image_array = til.roi_values_to_sitk_image_array(df, dimensions, modality)
+
+        sample = str(grp[0]) + '-' + str(grp[1])
+
+        write_image(image_array, sample, dir_output, suffix_output)
+
+
+def get_image_dimensions(path_to_image, tile_size=np.array([512, 512])):
+    size_full = util.get_image_size(path_to_image)
+    num_tiles, offset_tile = til.calculate_number_of_tiles(size_full, tile_size)
+    size_roi = np.array(num_tiles)*8
+
+    return size_roi
+
 
 def find_matching_image(group, dir_modality):
-    
+
     sample = str(group[0]) + '-' + str(group[1])
     path_image = [Path(image) for image in os.listdir(dir_modality) if image.startswith(sample)]
-    
-    return path_image
+
+    return path_image[0]
 
 
-def write_image(image_array, dir_output, suffix_output):
-    
+def write_image(image_array, sample, dir_output, suffix_output):
+
+    path_image = blk.create_new_image_path(sample, dir_output, suffix_output)
+
+    image = sitk.Cast(sitk.GetImageFromArray(np.transpose(image_array)), sitk.sitkFloat32)
+    sitk.WriteImage(image, str(path_image))
+
+
+
+dir_dict = dird.create_dictionary()
+path_averages = Path(dir_dict['anal'], 'ROIs_averaged_from_base_image.csv')
+df_averages = pd.read_csv(path_averages, header=[0, 1], index_col=[0, 1, 2, 3],
+                          dtype={'Mouse': object, 'Slide': object})
+bulk_construct_images(df_averages['Orientation', 'MLR-O'], 'MLR-O', dir_dict['mlr_large_reg'],
+                      dir_dict['images'], 'MLR-O_Averaged_Orientation')
