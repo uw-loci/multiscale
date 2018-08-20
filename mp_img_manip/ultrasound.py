381982a220bd2e33adb246282ee2e715a800da9f
mpinkert <mpinkert@wisc.edu>
2018-08-20 12:09:37 -0500

    SimpleITK save image for the 3D images

--- mp_img_manip/ultrasound.py
+++ mp_img_manip/ultrasound.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import numpy as np
 import mp_img_manip.utility_functions as util
 import re
+import SimpleITK as sitk
 
 
 def open_iq(path_iq: Path) -> np.ndarray:
@@ -86,13 +87,14 @@ def get_idx_img_z(idx_raw: int, num_xy: np.ndarray, num_imgs: int) -> [int, int]
     return int(img), int(z)
 
 
-def assemble_4d_image(list_mats, num_xy):
+def assemble_4d_image(list_mats: list, num_xy: np.ndarray) -> np.ndarray:
     """Compile US .mats into separate 3d images"""
     image_shape = np.shape(open_iq(list_mats[0]))
     num_imgs = len(list_mats)
 
     array_4d_im_z_yx = np.zeros([num_xy[0], num_xy[1], image_shape[0], image_shape[1]])
 
+    # todo: vectorize this loop
     for path in list_mats:
         idx_raw = index_from_file_path(path)
         img, z = get_idx_img_z(idx_raw, num_xy, num_imgs)
@@ -103,19 +105,23 @@ def assemble_4d_image(list_mats, num_xy):
     return array_4d_im_z_yx
 
 
-def stitch_us_image(dir_mats, path_pl, dir_output, name_output):
+def stitch_us_image(dir_mats: Path, path_pl: Path, dir_output: Path, name_output: str):
     list_mats = util.list_filetype_in_dir(dir_mats, 'mat')
     list_pos = read_position_list(path_pl)
     num_xy = count_xy_positions(list_pos)
     separate_images_4d = assemble_4d_image(list_mats, num_xy)
 
-    return separate_images_4d
-
-
+    for idx in range(num_xy[0]):
+        path_output = Path(dir_output, name_output + '_' + str(idx) + '.tif')
+        image = sitk.GetImageFromArray(separate_images_4d[idx])
+        image_cast = sitk.Cast(image, sitk.sitkFloat32)
+        sitk.WriteImage(image_cast, str(path_output))
 
 
 dir_mats = Path("C:\\Users\\mpinkert\\Box\\Research\\LINK\\Ultrasound\\Ultrasound Data\\2018-08-17\\PhantomGrid-TopLeftStart\\Run-2")
 path_pl = Path('C:\\Users\\mpinkert\\Box\\Research\\LINK\\Ultrasound\\Ultrasound Data\\2018-08-17\\US Phantom grid 2018-08-17.pos')
-stitch_us_image(dir_mats, path_pl, '', '')
+dir_output = Path('C:\\Users\\mpinkert\\Box\\Research\\LINK\\Ultrasound\\Ultrasound Data\\2018-08-17\\')
+name_output = 'FirstImage3D'
+stitch_us_image(dir_mats, path_pl, dir_output, name_output)
 
 
