e83db662951274c8ecc236e35e919cdf6340f5f0
mpinkert <mpinkert@wisc.edu>
2018-08-29 10:17:08 -0500

    Axial detrend implemented

--- mp_img_manip/ultrasound/correlation.py
+++ mp_img_manip/ultrasound/correlation.py
@@ -5,8 +5,9 @@ Organization: Laboratory for Optical and Computation Instrumentation, University
 """
 
 import numpy as np
+import scipy.signal as sig
 
-def define_correlation_window(dimens_window: np.ndarray) -> :
+def define_correlation_window(dimens_window: np.ndarray):
     """Define the window over which the correlation is calculated inside the frame"""
 
     params_window = {}
@@ -24,9 +25,14 @@ def determine_window_sweep():
     return
 
 
-def detrend_global():
-    """Detrend the entire 3D image"""
-    return
+def detrend_axial_global(array_im: np.ndarray, axial_dim: int=1, lateral_dim: int=0) -> np.ndarray:
+    """Detrend each column of the 3D image"""
+    array_detrend = sig.detrend(array_im, axis=axial_dim)
+    mean_detrend = np.mean(array_detrend, lateral_dim)
+
+    array_detrend = array_detrend - mean_detrend
+
+    return array_detrend
 
 
 def detrend_window():
