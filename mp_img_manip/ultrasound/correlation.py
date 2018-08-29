0f9d1249c250757e787ccf2e550d302c7d7e18ff
mpinkert <mpinkert@wisc.edu>
2018-08-29 13:39:37 -0500

    Untested detrend along dimension, only accounts for 3 dimensions

--- mp_img_manip/ultrasound/correlation.py
+++ mp_img_manip/ultrasound/correlation.py
@@ -7,6 +7,7 @@ Organization: Laboratory for Optical and Computation Instrumentation, University
 import numpy as np
 import scipy.signal as sig
 
+
 def define_correlation_window(dimens_window: np.ndarray):
     """Define the window over which the correlation is calculated inside the frame"""
 
@@ -25,6 +26,29 @@ def determine_window_sweep():
     return
 
 
+def detrend_along_dimension(array_im: np.ndarray, dim_detrend: int, dim_to_average: int) -> np.ndarray:
+    """Detrend along a dimension, and subtract a mean of the detrend along another dimension
+
+    axis 0 = elevation
+    axis 1 = axial
+    axis 2 = lateral
+    """
+
+    array_detrend = sig.detrend(array_im, axis=dim_detrend)
+    array_mean = np.mean(array_detrend, dim_to_average)
+
+    shape_array = np.shape(array_detrend)
+
+    if dim_to_average is 2:
+        array_detrend_mean = array_detrend - array_mean[:, :, None]
+    elif dim_to_average is 1:
+        array_detrend_mean = array_detrend - np.reshape(array_mean, [shape_array[0], 1, shape_array[2]])
+    elif dim_to_average is 0:
+        array_detrend_mean = array_detrend - array_mean
+
+    return array_detrend_mean
+
+
 def detrend_axial_global(array_im: np.ndarray, axial_dim: int=1, lateral_dim: int=2) -> np.ndarray:
     """Detrend along the axial dimension, for each lateral frame
 
@@ -32,6 +56,7 @@ def detrend_axial_global(array_im: np.ndarray, axial_dim: int=1, lateral_dim: in
     axis 0 = elevation
     axis 1 = axial
     axis 2 = lateral
+
     """
     array_detrend = sig.detrend(array_im, axis=axial_dim)
     mean_detrend = np.mean(array_detrend, lateral_dim)
