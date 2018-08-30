2d91da4bbca90f75f34a871ede2511358f808305
mpinkert <mpinkert@wisc.edu>
2018-08-30 14:26:48 -0500

    Metric plot now has good axis limits

--- mp_img_manip/itk/registration.py
+++ mp_img_manip/itk/registration.py
@@ -93,14 +93,14 @@ def affine_register(fixed_image, moving_image,
 
     # Connect all of the observers so that we can plot during registration.
     # registration_method.AddCommand(sitk.sitkStartEvent, start_plot)
-    # registration_method.AddCommand(sitk.sitkEndEvent, end_plot)
+    #     registration_method.AddCommand(sitk.sitkEndEvent, reg_plot.end_plot)
 
     reg_plot = RegistrationPlot(registration_method.GetMetricValue(),
                                 fixed_image, moving_image,
                                 transform)
     registration_method.AddCommand(sitk.sitkMultiResolutionIterationEvent,
                                    reg_plot.update_idx_resolution_switch)
-    registration_method.AddCommand(sitk.sitkIterationEvent, reg_plot.update_plot(
+    registration_method.AddCommand(sitk.sitkIterationEvent, lambda: reg_plot.update_plot(
         registration_method.GetMetricValue(), fixed_image, moving_image, transform))
 
     return (registration_method.Execute(fixed_image, moving_image),
--- mp_img_manip/itk/registration_plot.py
+++ mp_img_manip/itk/registration_plot.py
@@ -14,6 +14,8 @@ class RegistrationPlot:
 
         self.ax_cost.set_xlabel('Iteration Number', fontsize=12)
         self.ax_cost.set_ylabel('Metric Value', fontsize=12, rotation='90')
+        self.ax_cost.set_xlim(0, 1)
+        self.ax_cost.set_ylim(start_metric_value*2, 0)
 
         moving_transformed = sitk.Resample(moving_image, fixed_image, transform,
                                            sitk.sitkLinear, 0.0,
@@ -24,14 +26,16 @@ class RegistrationPlot:
 
         self.plot, = self.ax_cost.plot(self.metric_values, 'r')
         self.plot_multires, = self.ax_cost.plot(self.idx_resolution_switch,
-                                               [self.metric_values[index] for index in self.idx_resolution_switch],
-                                               'b*')
+                                                [self.metric_values[index] for index in self.idx_resolution_switch],
+                                                'b*')
 
     def update_plot(self, new_metric_value, fixed_image, moving_image, transform):
         """Event: Update and plot new registration values"""
 
-        self.metric_values = self.metric_values.append(new_metric_value)
-        self.plot.set_data(self.metric_values)
+        self.metric_values.append(new_metric_value)
+        self.plot.set_data(range(len(self.metric_values)), self.metric_values)
+        self.ax_cost.set_xlim(0, len(self.metric_values))
+        self.ax_cost.set_ylim(2*min(self.metric_values), 0)
 
         moving_transformed = sitk.Resample(moving_image, fixed_image, transform,
                                            sitk.sitkLinear, 0.0,
@@ -44,11 +48,13 @@ class RegistrationPlot:
         asp = np.diff(self.ax_cost.get_xlim())[0] / np.diff(self.ax_cost.get_ylim())[0]
         self.ax_cost.set_aspect(asp)
 
-        self.fig.draw()
+        # self.fig.draw()
 
     def update_idx_resolution_switch(self):
         new_idx = len(self.metric_values)
-        self.idx_resolution_switch = self.idx_resolution_switch.append(new_idx)
+        self.idx_resolution_switch.append(new_idx)
+
+
 
 
 
