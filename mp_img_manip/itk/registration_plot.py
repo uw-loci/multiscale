f9efe3d923947cc8366060df18b9a825dea0153d
mpinkert <mpinkert@wisc.edu>
2018-08-30 16:20:40 -0500

    X ticks only on integers

--- mp_img_manip/itk/registration_plot.py
+++ mp_img_manip/itk/registration_plot.py
@@ -2,20 +2,26 @@ import matplotlib.pyplot as plt
 import SimpleITK as sitk
 import mp_img_manip.itk.process as proc
 import numpy as np
+import matplotlib.ticker as plticker
 
 
 class RegistrationPlot:
-    def __init__(self, start_metric_value, moving_image, fixed_image, transform):
+    def __init__(self, start_metric_value, fixed_image, moving_image, transform):
         self.metric_values = [start_metric_value]
         self.idx_resolution_switch = [0]
         self.fig, (self.ax_img, self.ax_cost) = plt.subplots(1, 2)
 
+        self.fig.set_size_inches(16, 8)
+
         self.ax_img.axis('off')
 
         self.ax_cost.set_xlabel('Iteration Number', fontsize=12)
-        self.ax_cost.set_ylabel('Metric Value', fontsize=12, rotation='90')
+        self.ax_cost.set_title('Metric Value', fontsize=12)
         self.ax_cost.set_xlim(0, 1)
         self.ax_cost.set_ylim(start_metric_value*2, 0)
+
+        loc = plticker.MultipleLocator(base=1.0)  # this locator puts ticks at regular intervals
+        self.ax_cost.xaxis.set_major_locator(loc)
 
         moving_transformed = sitk.Resample(moving_image, fixed_image, transform,
                                            sitk.sitkLinear, 0.0,
@@ -48,7 +54,8 @@ class RegistrationPlot:
         asp = np.diff(self.ax_cost.get_xlim())[0] / np.diff(self.ax_cost.get_ylim())[0]
         self.ax_cost.set_aspect(asp)
 
-        # self.fig.draw()
+        plt.draw()
+        plt.pause(0.02)
 
     def update_idx_resolution_switch(self):
         new_idx = len(self.metric_values)
