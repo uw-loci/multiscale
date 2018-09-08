f9e4cc4971730fb57a4abf723e51e91016c34eb5
mpinkert <mpinkert@wisc.edu>
2018-09-08 11:54:03 -0500

    Task script to calculate US correlation

---
+++ mp_img_manip/ultrasound/task_scripts/calc_correlation.py
@@ -0,0 +1,4 @@
+import mp_img_manip.ultrasound.correlation as corr
+from pathlib import Path
+dir_rf = Path(r'C:\Users\mpinkert\Box\Research\LINK\Ultrasound\Ultrasound Data\2018-09-03\Rovyer_Close_14-5V\Run-5')
+corr.calc_plot_corr_curves(dir_rf)
