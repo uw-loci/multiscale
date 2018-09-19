23a3262de9f32bd1128ed82938db7ea60a3e2da1
mpinkert <mpinkert@wisc.edu>
2018-09-19 08:57:27 -0500

    Began fixture writing and normal detrend test

--- mp_img_manip/tests/test_us_correlation.py
+++ mp_img_manip/tests/test_us_correlation.py
@@ -1,9 +1,18 @@
 import numpy as np
 import scipy.signal as sig
 import mp_img_manip.ultrasound.correlation as corr
+import pytest
 
+
+@pytest.fixture(scope='module')
+def rand_array():
+    array = np.random.randn(3, 4, 5)
+    return array
 
 class TestDetrend(object):
+
+
+class TestDetrendAndSubtractMean(object):
     def test_detrend_axis_0_1(self):
         array_dummy = np.random.randn(3, 4, 5)
         array_detrend = sig.detrend(array_dummy, 0)
@@ -12,7 +21,7 @@ class TestDetrend(object):
         for i in range(4):
             array_looped[:, i, :] = array_detrend[:, i, :] - array_mean
 
-        array_from_func = corr.detrend_along_dimension(array_dummy, dim_detrend=0, dim_to_average=1)
+        array_from_func = corr.detrend_along_dimension_and_subtract_mean(array_dummy, dim_detrend=0, dim_to_average=1)
         assert (array_mean == array_from_func).all()
 
     def test_detrend_axis_0_2(self):
@@ -23,7 +32,7 @@ class TestDetrend(object):
         for i in range(5):
             array_looped[:, :, i] = array_detrend[:, :, i] - array_mean
 
-        array_from_func = corr.detrend_along_dimension(array_dummy, dim_detrend=0, dim_to_average=1)
+        array_from_func = corr.detrend_along_dimension_and_subtract_mean(array_dummy, dim_detrend=0, dim_to_average=1)
         assert (array_mean == array_from_func).all()
 
     def test_detrend_axis_1_0(self):
@@ -34,7 +43,7 @@ class TestDetrend(object):
         for i in range(4):
             array_looped[:, i, :] = array_detrend[:, i, :] - array_mean
 
-        array_from_func = corr.detrend_along_dimension(array_dummy, dim_detrend=0, dim_to_average=1)
+        array_from_func = corr.detrend_along_dimension_and_subtract_mean(array_dummy, dim_detrend=0, dim_to_average=1)
         assert (array_mean == array_from_func).all()
 
     def test_detrend_axis_1_2(self):
@@ -45,7 +54,7 @@ class TestDetrend(object):
         for i in range(4):
             array_looped[:, i, :] = array_detrend[:, i, :] - array_mean
 
-        array_from_func = corr.detrend_along_dimension(array_dummy, dim_detrend=0, dim_to_average=1)
+        array_from_func = corr.detrend_along_dimension_and_subtract_mean(array_dummy, dim_detrend=0, dim_to_average=1)
         assert (array_mean == array_from_func).all()
 
     def test_detrend_axis_2_0(self):
@@ -56,7 +65,7 @@ class TestDetrend(object):
         for i in range(4):
             array_looped[:, i, :] = array_detrend[:, i, :] - array_mean
 
-        array_from_func = corr.detrend_along_dimension(array_dummy, dim_detrend=0, dim_to_average=1)
+        array_from_func = corr.detrend_along_dimension_and_subtract_mean(array_dummy, dim_detrend=0, dim_to_average=1)
         assert (array_mean == array_from_func).all()
 
     def test_detrend_axis_2_1(self):
@@ -67,5 +76,5 @@ class TestDetrend(object):
         for i in range(4):
             array_looped[:, i, :] = array_detrend[:, i, :] - array_mean
 
-        array_from_func = corr.detrend_along_dimension(array_dummy, dim_detrend=0, dim_to_average=1)
+        array_from_func = corr.detrend_along_dimension_and_subtract_mean(array_dummy, dim_detrend=0, dim_to_average=1)
         assert (array_mean == array_from_func).all()
