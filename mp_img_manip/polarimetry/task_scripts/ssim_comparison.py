9c1648f8403657bc84cf1bd248e6a909805b2b2b
mpinkert <mpinkert@wisc.edu>
2018-10-01 10:49:37 -0500

    renamed ssim task script as conflicted with package name

--- mp_img_manip/polarimetry/task_scripts/ssim_comparison.py
+++ mp_img_manip/polarimetry/task_scripts/ssim_comparison.py
@@ -5,7 +5,7 @@ Created on Fri Jul 13 13:00:47 2018
 @author: mpinkert
 """
 
-import mp_img_manip.cw_ssim as ssim
+import mp_img_manip.cw_ssim as cw_ssim
 import mp_img_manip.polarimetry.dir_dictionary as dird
 import datetime
 
@@ -21,7 +21,7 @@ output_name_small = 'CW_SSIM_Small_' + date + '.csv'
 output_name = 'CW-SSIM_' + date + '.csv'
 
 
-ssim.bulk_compare_ssim(mlr_mhr_small, dir_dict['ssim'], output_name)
+cw_ssim.bulk_compare_ssim(mlr_mhr_small, dir_dict['ssim'], output_name)
 
-ssim.bulk_compare_ssim(mlr_mhr, dir_dict['ssim'], output_name)
+cw_ssim.bulk_compare_ssim(mlr_mhr, dir_dict['ssim'], output_name)
 
