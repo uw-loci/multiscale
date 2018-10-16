dc0caaaf1ec681edfb0f38d2b9d1a13af26b3db8
mpinkert <mpinkert@wisc.edu>
2018-10-16 10:15:30 -0500

    Make task script paramaters.csv -> meta.txt+tfm

---
+++ mp_img_manip/itk/task_scripts/parameters_to_individual_metadata.py
@@ -0,0 +1,6 @@
+"""
+This script converts from the deprecated method of writing a single image parameters file holder both initial
+transform and image properties such as spacing, to individual files holding the metadata and the initial transform.
+
+The metadata is a custom txt file but the initial transform is native SITK
+"""
