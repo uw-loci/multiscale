632dbb461cb52d93cc1a89ddaf8e816a8a17d32d
mpinkert <mpinkert@wisc.edu>
2018-10-17 16:18:03 -0500

    Change Try/Except to if/else in read_initial_tran
    This change is because the try triggered some very long warnings,
    even though it did carry through.

--- mp_img_manip/itk/transform.py
+++ mp_img_manip/itk/transform.py
@@ -227,10 +227,10 @@ def read_initial_transform(path_image: Path, transform_type: type):
         
         path_transform = Path(path_image.parent, path_image.stem + '_initial.tfm')
         
-        try:
+        if path_transform.is_file():
                 transform = sitk.ReadTransform(str(path_transform))
-        except:
-                print('No transform found.  Writing transform for {0}'.format(path_image.name))
+        else:
+                print('No transform found.  Writing blank transform for {0}'.format(path_image.name))
                 transform = define_transform(transform_type)
                 sitk.WriteTransform(transform, str(path_transform))
         
