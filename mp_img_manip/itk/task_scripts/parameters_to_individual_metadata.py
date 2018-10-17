96a03058ae0481490f2c486cddf99e858b1cea23
mpinkert <mpinkert@wisc.edu>
2018-10-17 09:40:01 -0500

    Implement func adjusts spacing from parameters.csv
    This function also includes non-working code to adjust the image
    metadata - unforuntately, simpleITK cannot write tif metadata so I need
    to develop alternate methods going forward.

--- mp_img_manip/itk/task_scripts/parameters_to_individual_metadata.py
+++ mp_img_manip/itk/task_scripts/parameters_to_individual_metadata.py
@@ -9,5 +9,30 @@ import SimpleITK as sitk
 from pathlib import Path
 import pandas as pd
 
+
 def parameters_file_to_metadata(dir_params):
         path_params = Path(dir_params, 'Image Parameters.csv')
+        df_params = pd.read_csv(path_params, index_col=0)
+        
+        for idx in df_params.index:
+                image_parameters = df_params.loc[idx]
+                path_image= Path(dir_params, idx)
+                image = sitk.ReadImage(str(path_image))
+                
+                if image.GetSpacing()[0] == 1.0:
+                        print('Adjusting {0}'.format(idx))
+                        unit = 'microns'
+                        
+                        spacing = [float(image_parameters['Spacing']),
+                                   float(image_parameters['Spacing'])]
+                        image.SetSpacing(spacing)
+                        image.SetMetaData('Unit', unit)
+                        
+                        writer = sitk.ImageFileWriter()
+                        writer.SetFileName(str(path_image))
+                        writer.Execute(image)
+        
+                
+dir_params = r'F:\Research\Polarimetry\Data 02 - Python prepped images\SHG_Large'
+
+parameters_file_to_metadata(dir_params)
