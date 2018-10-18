d284ff5f6c18c6ab2db780ac14a0fce3d996daf0
mpinkert <mpinkert@wisc.edu>
2018-10-18 12:23:08 -0500

    Plotting code exploration manual tests

---
+++ mp_img_manip/tests/manual_test/trivial_qt_draw.py
@@ -0,0 +1,17 @@
+import matplotlib as mpl
+mpl.use('qt5agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 6*np.pi, 100)
+y = np.sin(x)
+
+plt.ion()
+
+fig, ax = plt.subplots()
+line1, = ax.plot(x, y, 'r-')
+
+for phase in np.linspace(0, 10*np.pi, 500):
+    line1.set_ydata(np.sin(x + phase))
+    fig.canvas.draw()
+    fig.canvas.flush_events()
