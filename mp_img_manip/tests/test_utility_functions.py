c76699628ae34b574ca67ea84a31585749cc8403
mpinkert <mpinkert@wisc.edu>
2018-10-19 15:01:23 -0500

    Make test and fixture for item_present_all_lists

--- mp_img_manip/tests/test_utility_functions.py
+++ mp_img_manip/tests/test_utility_functions.py
@@ -1,3 +1,27 @@
 import pytest
 import mp_img_manip.utility_functions as util
 
+
+@pytest.fixture()
+def list_of_file_lists():
+        """
+        Returns a list of file lists containing lists for different conditions
+        :return:
+        """
+        list_one = ['a', 'b.tif', 'c.png', 'd']
+        list_two = ['a', 'b.tif', 'c.test']
+        list_three =['a', 'b', 'c.tif', {'Test': 'Dict'}]
+        lists = [list_one, list_two, list_three]
+        return lists
+
+
+class TestItemPresentAllLists(object):
+        @pytest.mark.parametrize("item, expected", [
+                ('a', True),
+                ('b', False),
+                ('b.tif', False),
+                ({'Test': 'Dict'}, False)
+        ])
+        def test_item_present(self, list_of_file_lists, item, expected):
+                assert util.item_present_all_lists(item, list_of_file_lists) == expected
+
