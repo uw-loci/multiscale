2ad75b382e0c6b974fff4bbaaa5fd7cac3984cda
mpinkert <mpinkert@wisc.edu>
2018-10-20 11:10:30 -0500

    Test for character_indices

--- mp_img_manip/tests/test_utility_functions.py
+++ mp_img_manip/tests/test_utility_functions.py
@@ -5,11 +5,13 @@ import mp_img_manip.utility_functions as util
 @pytest.fixture()
 def user_input_fixture(monkeypatch):
         monkeypatch.setattr('builtins.input', lambda x: next(x))
+
         def _user_inputs(inputs):
                 for item in inputs:
                         yield item
                 
         return _user_inputs
+
 
 @pytest.fixture()
 def list_of_file_lists():
@@ -23,6 +25,9 @@ def list_of_file_lists():
         lists = [list_one, list_two, list_three]
         return lists
 
+class TestWriteDict(object):
+        def test_write_object(self):
+                assert True
 
 class TestItemPresentAllLists(object):
         @pytest.mark.parametrize("item, expected", [
@@ -37,7 +42,7 @@ class TestQueryInt(object):
                 monkeypatch.setattr('builtins.input', lambda x: '5')
                 assert util.query_int('') == 5
                 
-        def test_user_gives_wrong_inputs_then_int(self, monkeypatch, user_input_fixture):
+        def test_user_gives_wrong_inputs_then_int(self, user_input_fixture):
                 user_inputs = user_input_fixture(['5.4', 'eight', 'a5', '5a', '5'])
                 assert util.query_int(user_inputs) == 5
                 
@@ -51,7 +56,7 @@ class TestQueryFloat(object):
                 monkeypatch.setattr('builtins.input', lambda x: '5')
                 assert util.query_float('') == 5.0
 
-        def test_user_gives_wrong_inputs_then_float(self, monkeypatch, user_input_fixture):
+        def test_user_gives_wrong_inputs_then_float(self, user_input_fixture):
                 user_inputs = user_input_fixture(['eight', 'a5', '5a', '5.4'])
                 assert util.query_float(user_inputs) == 5.4
                 
@@ -63,8 +68,22 @@ class TestYesNo(object):
         ])
         def test_yes_no_inputs(self, input, expected, monkeypatch):
                 monkeypatch.setattr('builtins.input', lambda x: input)
-                assert util.yes_no('') == expected
+                assert util.yes_no('') is expected
         
         def test_bad_input_then_yes(self, monkeypatch, user_input_fixture):
                 user_inputs = user_input_fixture(['5', 'y', 'test'])
-                assert util.yes_no(user_inputs) == True
+                assert util.yes_no(user_inputs) is True
+
+
+class TestCharacterIndices(object):
+        @pytest.mark.parametrize('string, character, indices', [
+                ('aabcda', 'a', [0, 1, 5]),
+                ('z12dc1', '1', [1, 5]),
+                ('aa\ndc', '\n', [2]),
+                ('dd\u0061adc', 'a', [2, 3]),
+                ('abc\u0394d\u0394', '\u0394', [3, 5])
+        ])
+        def test_find_char(self, string, character, indices):
+                result = util.character_indices(string, character)
+                assert result == indices
+
