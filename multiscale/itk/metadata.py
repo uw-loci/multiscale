bdf8519264063b3e51c01b73572353f66205a0e9
mpinkert <mpinkert@wisc.edu>
2018-10-23 10:08:47 -0500

    Add [y/n] prompt to clarify query

--- multiscale/itk/metadata.py
+++ multiscale/itk/metadata.py
@@ -98,7 +98,8 @@ def setup_image(path_image: Path, unit_workspace: str='microns', write_changes: 
                 print('{0} has no metadata.'.format(str(path_image.name)))
                 image.SetMetaData('Unit', unit_workspace)
                 current_spacing = image.GetSpacing()
-                change_spacing = util.query_yes_no('Change the spacing?  Current spacing is {0} >> '.format(current_spacing))
+                change_spacing = util.query_yes_no('Change the spacing?  Current spacing is {0} [y/n] >> '
+                                                   .format(current_spacing))
                 
                 if change_spacing:
                         spacing = util.query_float('Please enter the image spacing in {0} >> '.format(unit_workspace))
