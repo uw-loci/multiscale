4fbc980096ea264c9700361c91ee0dfaf0e3d5ca
mpinkert <mpinkert@wisc.edu>
2018-10-25 09:23:28 -0500

    Start an image_classes module for LINK
    This module will contain classes for each individual measurement on
    LINK, SHG/US/EBS/OCT, as well as a super class that contains all of
    them.  Together, this should let us unify image procesing into a
    cohesive framework.

---
+++ multiscale/LINK_system/image_classes.py
@@ -0,0 +1,23 @@
+class UltrasoundImage(object):
+        def __init__(self):
+                return
+
+class SHGImage(object):
+        def __init__(self):
+                return
+        
+class EBSMeasurement(object):
+        def __init__(self):
+                return
+        
+class OCTImage(object):
+        def __init__(self):
+                return
+
+class LinkImage(object):
+        def __init__(self):
+                self.us_image = None
+                self.shg_image = None
+                self.ebs_measurement = None
+                self.oct_image = None
+                
