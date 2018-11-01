2bf916218937c29084a0d4a371db0da385d5e69a
mpinkert <mpinkert@wisc.edu>
2018-11-01 10:43:21 -0500

    Copy better loadmat from stackexchange
    The scipy loadmat gives a messy numpy array for cell structures, instead
    of a dictionary.  This copied function formats it into a clean dict that
    can be easily called/used.
    
    In addition, I've added a variable "variables" so that you can select
    certain variables to load from the mat.

--- multiscale/utility_functions.py
+++ multiscale/utility_functions.py
@@ -8,6 +8,8 @@ import os
 from pathlib import Path
 import shutil
 import json
+import scipy.io as sio
+import numpy as np
 
 
 def write_json(dictionary: dict, path_dict: Path):
@@ -131,4 +133,61 @@ def split_list_into_sublists(large_list: list, size_of_sublist: int):
         for i in range(0, len(large_list), size_of_sublist):
                 yield large_list[i:i + size_of_sublist]
 
+
+def load_mat(file_path, variables=None):
+        '''
+            this function should be called instead of direct spio.loadmat
+            as it cures the problem of not properly recovering python dictionaries
+            from mat files. It calls the function check keys to cure all entries
+            which are still mat-objects
+            '''
+
+        file_name = str(file_path)
+
+        def _check_keys(d):
+                '''
+                checks if entries in dictionary are mat-objects. If yes
+                todict is called to change them to nested dictionaries
+                '''
+                for key in d:
+                        if isinstance(d[key], sio.matlab.mio5_params.mat_struct):
+                                d[key] = _todict(d[key])
+                return d
+
+        def _todict(matobj):
+                '''
+                A recursive function which constructs from matobjects nested dictionaries
+                '''
+                d = {}
+                for strg in matobj._fieldnames:
+                        elem = matobj.__dict__[strg]
+                        if isinstance(elem, sio.matlab.mio5_params.mat_struct):
+                                d[strg] = _todict(elem)
+                        elif isinstance(elem, np.ndarray):
+                                d[strg] = _tolist(elem)
+                        else:
+                                d[strg] = elem
+                return d
+
+        def _tolist(ndarray):
+                '''
+                A recursive function which constructs lists from cellarrays
+                (which are loaded as numpy ndarrays), recursing into the elements
+                if they contain matobjects.
+                '''
+                elem_list = []
+                for sub_elem in ndarray:
+                        if isinstance(sub_elem, sio.matlab.mio5_params.mat_struct):
+                                elem_list.append(_todict(sub_elem))
+                        elif isinstance(sub_elem, np.ndarray):
+                                elem_list.append(_tolist(sub_elem))
+                        else:
+                                elem_list.append(sub_elem)
+                return elem_list
+
+        if variables is None:
+                data = sio.loadmat(file_name, struct_as_record=False, squeeze_me=True)
+        else:
+                data = sio.loadmat(file_name, struct_as_record=False, squeeze_me=True, variable_names=variables)
+        return _check_keys(data)
 
