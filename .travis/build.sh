34387c7fb7091b0cd4dd6614a6a83c896786c174
mpinkert <mpinkert@wisc.edu>
2018-11-13 15:58:36 -0600

    Test migrating travis commands to build.sh

---
+++ .travis/build.sh
@@ -0,0 +1,68 @@
+!/bin/bash
+
+sudo apt-get update
+
+# -- create a test enviroment --
+conda create -q -n multiscale python=$TRAVIS_PYTHON_VERSION
+source activate multiscale
+
+# -- install dependencies --
+conda install -c simpleitk SimpleITK
+conda install cython
+conda install h5py
+conda install -c conda-forge pyjnius
+conda install -c hanslovsky imglyb
+conda install pytest
+conda install pandas
+conda install scipy
+conda install matplotlib
+conda install pillow
+pip install imagej
+pip install pyssim
+pip install javabridge
+pip install python-bioformats
+
+pip install -r requirements.txt
+
+# -- install supporting tools --
+sudo apt -y install curl
+sudo apt -y install git
+sudo apt -y install unzip
+
+cd
+
+# -- download Fiji.app --
+if [ ! -d Fiji.app ]
+then
+  echo
+  echo "--> Downloading Fiji"
+  curl -fsO http://downloads.imagej.net/fiji/latest/fiji-nojre.zip
+
+  echo "--> Unpacking Fiji"
+  rm -rf Fiji.app
+  unzip fiji-nojre.zip
+fi
+
+# -- Determine correct ImageJ launcher executable --
+case "$(uname -s),$(uname -m)" in
+  Linux,x86_64) launcher=ImageJ-linux64 ;;
+  Linux,*) launcher=ImageJ-linux32 ;;
+  Darwin,*) launcher=Contents/MacOS/ImageJ-macosx ;;
+  MING*,*) launcher=ImageJ-win32.exe ;;
+  *) die "Unknown platform" ;;
+esac
+
+echo
+echo "--> Updating Fiji"
+Fiji.app/$launcher --update update-force-pristine
+
+# -- run the Python code --
+cd $TRAVIS_BUILD_DIR
+
+# -- set ij dirctory --
+ij_dir=$HOME/Fiji.app
+echo "ij_dir = $ij_dir"
+python setup.py install
+
+# -- run test with debug flag --
+python -m pytest --ij="$ij_dir"
