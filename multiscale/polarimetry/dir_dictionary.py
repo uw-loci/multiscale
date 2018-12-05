359a6f7e3f55b097cc117356b288500ad2ff8a20
mpinkert <mpinkert@wisc.edu>
2018-12-05 15:49:07 -0600

    Added PS dirs back

--- multiscale/polarimetry/dir_dictionary.py
+++ multiscale/polarimetry/dir_dictionary.py
@@ -67,6 +67,13 @@ def create_dictionary(
                 'mhr_large_reg_orient': os.path.join(base_dir, reg_dir, 'MHR_Large_Reg_Orient'),
                 "he_large_reg": os.path.join(base_dir, reg_dir, 'HE_Large_Reg'),
                 
+                "ps": os.path.join(base_dir, prep_dir, 'PS_Large'),
+                "ps_orient": os.path.join(base_dir, prep_dir, 'PS_Large_Orient'),
+                "ps_reg": os.path.join(base_dir, reg_dir, 'PS_Large_Reg'),
+                "ps_reg_raw": os.path.join(base_dir, reg_dir, 'PS_Large_Reg', 'Raw'),
+                "ps_reg_orient": os.path.join(base_dir, reg_dir, 'PS_Large_Reg_Orient'),
+                "ps_reg_orient_raw": os.path.join(base_dir, reg_dir, 'PS_Large_Reg_Orient', 'Raw'),
+        
                 #"mlr_large_mask": os.path.join(base_dir, mask_dir, 'MLR_Large_Mask'),
                 #"mhr_large_mask": os.path.join(base_dir, mask_dir, 'MHR_Large_Mask'),
                 #"mlr_large_mask_orient": os.path.join(base_dir, mask_dir, 'MLR_Large_Mask_Orient'),
