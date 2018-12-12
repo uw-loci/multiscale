02e68fd84b5878e968bfccedfef6369842dc9c58
mpinkert <mpinkert@wisc.edu>
2018-12-12 12:19:08 -0600

    Install Fiji before activating environment
    This is because Travis was throwing an error where it could not find
    the JVM library for ImageJ, due to the activated conda environment.

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,10 +4,7 @@ set -e
 
 sudo apt-get update
 
-# -- create a test enviroment --
-conda env create -q -f environment.yml
-source activate multiscale
-conda install -q -y python=$TRAVIS_PYTHON_VERSION
+
 
 cd
 
@@ -35,16 +32,22 @@ esac
 
 echo
 echo "--> Updating Fiji"
-# Fiji.app/$launcher --update edit-update-site BigStitcher https://sites.imagej.net/BigStitcher/
-# Fiji.app/$launcher --update update
+Fiji.app/$launcher --update edit-update-site BigStitcher https://sites.imagej.net/BigStitcher/
+Fiji.app/$launcher --update update
 echo "--> Finished updating Fiji"
-
-# -- run the Python code --
-cd $TRAVIS_BUILD_DIR
 
 # -- set ij dirctory --
 ij_dir=$HOME/Fiji.app
 echo "ij_dir = $ij_dir"
+
+# -- run the Python code --
+cd $TRAVIS_BUILD_DIR
+
+# -- create a test enviroment --
+conda env create -q -f environment.yml
+source activate multiscale
+conda install -q -y python=$TRAVIS_PYTHON_VERSION
+
 python setup.py install
 
 # -- run test with debug flag --
