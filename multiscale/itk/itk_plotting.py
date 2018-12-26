6261c44517f11fbaa8c7b5a37d92933a70f7f818
mpinkert <mpinkert@wisc.edu>
2018-12-26 15:56:44 -0600

    Copy MultiImageDisplay from sitk gui.py
    [CI no]

--- multiscale/itk/itk_plotting.py
+++ multiscale/itk/itk_plotting.py
@@ -3,6 +3,7 @@ import SimpleITK as sitk
 import ipywidgets as widgets
 import numpy as np
 import matplotlib.ticker as plticker
+from IPython.display import display, clear_output
 
 import multiscale.itk.transform as trans
 import multiscale.itk.metadata as meta
@@ -149,3 +150,125 @@ def overlay_images(fixed_image: sitk.Image, moving_image: sitk.Image):
 class OverlayWidget(object):
         def __init__(self):
                 return
+        
+        def temp(self):
+                return
+        
+
+class MultiImageDisplay(object):
+        """
+        Class for displaying multiple images side by side, with sliders, in Ipython. Adapted from SimpleITK notebooks gui.py
+        https://github.com/InsightSoftwareConsortium/SimpleITK-Notebooks/blob/master/Python/gui.py
+        """
+        def __init__(self, image_list, axis=0, shared_slider=False, title_list=None, window_level_list=None,
+                     figure_size=(9, 6), horizontal=True):
+                self.get_window_level_numpy_array(image_list, window_level_list)
+                if title_list:
+                        if len(image_list) != len(title_list):
+                                raise ValueError('Title list and image list lengths do not match')
+                        self.title_list = list(title_list)
+                else:
+                        self.title_list = [''] * len(image_list)
+                
+                # Our dynamic slice, based on the axis the user specifies
+                self.slc = [slice(None)] * 3
+                self.axis = axis
+                
+                ui = self.create_ui(shared_slider)
+                display(ui)
+                
+                # Create a figure.
+                col_num, row_num = (len(image_list), 1) if horizontal else (1, len(image_list))
+                self.fig, self.axes = plt.subplots(row_num, col_num, figsize=figure_size)
+                if len(image_list) == 1:
+                        self.axes = [self.axes]
+                
+                # Display the data and the controls, first time we display the image is outside the "update_display" method
+                # as that method relies on the previous zoom factor which doesn't exist yet.
+                for ax, npa, slider, min_intensity, max_intensity in zip(self.axes, self.npa_list, self.slider_list,
+                                                                         self.min_intensity_list, self.max_intensity_list):
+                        self.slc[self.axis] = slice(slider.value, slider.value + 1)
+                        # Need to use squeeze to collapse degenerate dimension (e.g. RGB image size 124 124 1 3)
+                        ax.imshow(np.squeeze(npa[tuple(self.slc)]),
+                                  cmap=plt.cm.Greys_r,
+                                  vmin=min_intensity,
+                                  vmax=max_intensity)
+                self.update_display()
+                plt.tight_layout()
+        
+        def create_ui(self, shared_slider):
+                # Create the active UI components. Height and width are specified in 'em' units. This is
+                # a html size specification, size relative to current font size.
+                ui = None
+                
+                if shared_slider:
+                        # Validate that all the images have the same size along the axis which we scroll through
+                        sz = self.npa_list[0].shape[self.axis]
+                        for npa in self.npa_list:
+                                if npa.shape[self.axis] != sz:
+                                        raise ValueError(
+                                                'Not all images have the same size along the specified axis, cannot share slider.')
+                        
+                        slider = widgets.IntSlider(description='image slice:',
+                                                   min=0,
+                                                   max=sz - 1,
+                                                   step=1,
+                                                   value=int((sz - 1) / 2),
+                                                   width='20em')
+                        slider.observe(self.on_slice_slider_value_change, names='value')
+                        self.slider_list = [slider] * len(self.npa_list)
+                        ui = widgets.Box(padding=7, children=[slider])
+                else:
+                        self.slider_list = []
+                        for npa in self.npa_list:
+                                slider = widgets.IntSlider(description='image slice:',
+                                                           min=0,
+                                                           max=npa.shape[self.axis] - 1,
+                                                           step=1,
+                                                           value=int((npa.shape[self.axis] - 1) / 2),
+                                                           width='20em')
+                                slider.observe(self.on_slice_slider_value_change, names='value')
+                                self.slider_list.append(slider)
+                        ui = widgets.Box(padding=7, children=self.slider_list)
+                return ui
+        
+        def get_window_level_numpy_array(self, image_list, window_level_list):
+                # Using GetArray and not GetArrayView because we don't keep references
+                # to the original images. If they are deleted outside the view would become
+                # invalid, so we use a copy wich guarentees that the gui is consistent.
+                self.npa_list = list(map(sitk.GetArrayFromImage, image_list))
+                if not window_level_list:
+                        self.min_intensity_list = list(map(np.min, self.npa_list))
+                        self.max_intensity_list = list(map(np.max, self.npa_list))
+                else:
+                        self.min_intensity_list = list(map(lambda x: x[1] - x[0] / 2.0, window_level_list))
+                        self.max_intensity_list = list(map(lambda x: x[1] + x[0] / 2.0, window_level_list))
+        
+        def on_slice_slider_value_change(self, change):
+                self.update_display()
+        
+        def update_display(self):
+                # Draw the image(s)
+                for ax, npa, title, slider, min_intensity, max_intensity in zip(self.axes, self.npa_list, self.title_list,
+                                                                                self.slider_list, self.min_intensity_list,
+                                                                                self.max_intensity_list):
+                        # We want to keep the zoom factor which was set prior to display, so we log it before
+                        # clearing the axes.
+                        xlim = ax.get_xlim()
+                        ylim = ax.get_ylim()
+                        
+                        self.slc[self.axis] = slice(slider.value, slider.value + 1)
+                        ax.clear()
+                        # Need to use squeeze to collapse degenerate dimension (e.g. RGB image size 124 124 1 3)
+                        ax.imshow(np.squeeze(npa[tuple(self.slc)]),
+                                  cmap=plt.cm.Greys_r,
+                                  vmin=min_intensity,
+                                  vmax=max_intensity)
+                        ax.set_title(title)
+                        ax.set_axis_off()
+                        
+                        # Set the zoom factor back to what it was before we cleared the axes, and rendered our data.
+                        ax.set_xlim(xlim)
+                        ax.set_ylim(ylim)
+                
+                self.fig.canvas.draw_idle()
